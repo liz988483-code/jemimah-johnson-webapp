fe2bc0cc288791872e49ed61e0b7aed0a9994218
Elizabeth Maina <elizamaina4447@gmail.com>
2026-05-15 11:32:33 +0300

    update Home page content

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import HeroSection from '@/components/home/HeroSection'
 import ServicesOverview from '@/components/home/ServicesOverview'
-import CompanyRegistrationPackages from '@/components/home/CompanyRegistrationPackages'
 import ContactCTA from '@/components/home/ContactCTA'
 
 const Home: React.FC = () => {
@@ -9,7 +8,6 @@ const Home: React.FC = () => {
     <div>
       <HeroSection />
       <ServicesOverview />
-      <CompanyRegistrationPackages />
       <ContactCTA />
     </div>
   )
